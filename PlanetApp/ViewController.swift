4958a26b7481b54332aad4b885b3e4a5c5f939dc
H8K-iOS <alexalimov17@gmail.com>
2023-10-14 16:02:37 +0200

    Initial Commit

---
+++ PlanetApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PlanetApp
+//
+//  Created by Alexandr Alimov on 14/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
